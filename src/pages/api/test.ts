42a5f2483ad3e627327640c576129802acb5feb7
inpiniti <inpiniti@gmail.com>
2024-11-17 22:55:56 +0900

    db 에서 데이터를 가져와서 조회하기

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import db from "@/lib/db"; // import the db connection
+import { nasdaqLiveTable } from '@/db/schema';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if(req.method == 'GET') {
-        res.status(200).json('data gkgkgkkgkks');
+        const result = await db.select().from(nasdaqLiveTable); // 'users' 테이블에서 모든 데이터를 선택
+        res.status(200).json(result);
     } else {
         res.status(405).json({error: 'Method Not Allowed'});
     }
