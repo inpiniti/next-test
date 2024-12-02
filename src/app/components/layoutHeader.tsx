cd1dfe3a026044661c3cf5f9e8fd5c43aa94d685
ykjung <ykjung@scgs.co.kr>
2024-12-03 08:56:57 +0900

    fix: ESLint 규칙

--- src/app/components/layoutHeader.tsx
+++ src/app/components/layoutHeader.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
