7ebd7b9da9c0a678600a8417ff823256fc2521ef
ykjung <ykjung@scgs.co.kr>
2024-12-03 08:54:53 +0900

    feat: 상단에 구현할 기능 추가

--- src/app/components/layoutHeader.tsx
+++ src/app/components/layoutHeader.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { SidebarTrigger } from '@/components/ui/sidebar';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 import useFilterStore from '@/stores/useFilterStore';
 
@@ -16,9 +17,21 @@ export default function LayoutHeader() {
         onChange={(e) => setFilter({ ...filter, stock: e.target.value })}
         placeholder="종목검색"
       />
-      <Button>전체</Button>
-      <Button variant="secondary">구매 목록 (미구현)</Button>
-      <Button variant="secondary">관심 종목 (미구현)</Button>
+      <Tabs defaultValue="account">
+        <TabsList className="grid w-full grid-cols-5">
+          <TabsTrigger value="account">전종목</TabsTrigger>
+          <TabsTrigger value="password" disabled>
+            구매 목록
+          </TabsTrigger>
+          <TabsTrigger value="password2" disabled>
+            관심 종목
+          </TabsTrigger>
+          <TabsTrigger value="password3" disabled>
+            종목 토론
+          </TabsTrigger>
+          <TabsTrigger value="password4">테스트</TabsTrigger>
+        </TabsList>
+      </Tabs>
     </div>
   );
 }
