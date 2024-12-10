9d3981c1673041612b41981b3de3172482ae26ed
ykjung <ykjung@scgs.co.kr>
2024-12-10 19:53:56 +0900

    feat: 카카오 로그인 리다이렉트

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -5,10 +5,15 @@ export const useAuth = () => {
   const supabase = useSupabase();
 
   const login = async () => {
+    const redirectTo =
+      process.env.NODE_ENV === 'production'
+        ? 'https://stock.potatomap.com'
+        : 'http://localhost:3000';
+
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'kakao',
       options: {
-        redirectTo: 'http://localhost:3000',
+        redirectTo,
       },
     });
     if (error) {
