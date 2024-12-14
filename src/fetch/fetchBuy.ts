9181d7a603e8e170b609f65c223b3d4420d995d0
inpiniti <inpiniti@gmail.com>
2024-12-14 18:34:10 +0900

    feat: 네트워크 안됐을때 임시 데이터 내려받도록

--- src/fetch/fetchBuy.ts
+++ src/fetch/fetchBuy.ts
@@ -1,57 +1,62 @@
-import { TBuy } from '@/interface/TBuy';
+import { TBuy } from "@/interface/TBuy";
 
 export const fetchBuy = {
   get: async (id: string) => {
-    const response = await fetch(`api/buy?id=${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (!response.ok) {
-      throw new Error('네트워크 응답이 올바르지 않습니다.');
+    try {
+      const response = await fetch(`api/buy?id=${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        return [{}];
+      } else {
+        const data = await response.json();
+        return data;
+      }
+    } catch {
+      return {};
     }
-    const data = await response.json();
-    return data;
   },
   post: async (body: TBuy) => {
     const response = await fetch(`api/buy`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(body),
     });
     if (!response.ok) {
-      throw new Error('네트워크 응답이 올바르지 않습니다.');
+      throw new Error("네트워크 응답이 올바르지 않습니다.");
     }
     const data = await response.json();
     return data;
   },
   put: async (body: TBuy) => {
     const response = await fetch(`api/buy`, {
-      method: 'PUT',
+      method: "PUT",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(body),
     });
     if (!response.ok) {
-      throw new Error('네트워크 응답이 올바르지 않습니다.');
+      throw new Error("네트워크 응답이 올바르지 않습니다.");
     }
     const data = await response.json();
     return data;
   },
   delete: async (body: TBuy) => {
     const response = await fetch(`api/buy`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(body),
     });
     if (!response.ok) {
-      throw new Error('네트워크 응답이 올바르지 않습니다.');
+      throw new Error("네트워크 응답이 올바르지 않습니다.");
     }
     const data = await response.json();
     return data;
