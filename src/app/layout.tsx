5e1c39faab7ad5b25d218fd409bb35c96f0d78a1
ykjung <ykjung@scgs.co.kr>
2024-12-26 09:03:36 +0900

    fix: google search console 에서 <script type="application/ld+json"> 이 부분이 에러 남

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,30 @@
-"use client";
+'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import localFont from "next/font/local";
-import "./globals.css";
+import localFont from 'next/font/local';
+import './globals.css';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
-import { SidebarProvider } from "@/components/ui/sidebar";
-import LayoutHeader from "./components/layoutHeader";
-import LayoutSidebar from "./components/layoutSidebar";
-import LayoutDialog from "./components/layoutDialog";
-import { LayoutASidebar } from "./components/layoutASidebar";
+import { SidebarProvider } from '@/components/ui/sidebar';
+import LayoutHeader from './components/layoutHeader';
+import LayoutSidebar from './components/layoutSidebar';
+import LayoutDialog from './components/layoutDialog';
+import { LayoutASidebar } from './components/layoutASidebar';
 
-import useFilterStore from "@/stores/useFilterStore";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { ThemeProvider } from "@/components/theme-provider";
+import useFilterStore from '@/stores/useFilterStore';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export default function RootLayout({
   children,
@@ -63,18 +63,6 @@ export default function RootLayout({
           name="twitter:card"
           content="https://images.unsplash.com/photo-1621264437251-59d700cfb327?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         />
-        <script type="application/ld+json">
-          {`
-            {
-              "@context": "https://stock.potatomap.com",
-              "@type": "WebSite",
-              "name": "감자증권",
-              "url": "https://stock.potatomap.com",
-              "description": "AI를 사용하여 1시간 뒤부터 1년까지의 변동율을 예측하고, 소팅 및 다양한 필터를 통해 데이터를 분석할 수 있는 사이트입니다.",
-            }
-          `}
-        </script>
-        {/* 기타 head 요소 */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
