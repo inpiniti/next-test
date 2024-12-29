7acdffa0e616c657f08bd558b73284044cd96df9
inpiniti <inpiniti@gmail.com>
2024-12-29 10:03:30 +0900

    fix: update db connection calls to use function invocation in toss table API

--- src/pages/api/toss/table.ts
+++ src/pages/api/toss/table.ts
@@ -2,11 +2,11 @@ import db from "@/lib/db"; // import the db connection
 import { tossTable } from "@/db/tossTable";
 
 export const select = async () => {
-  const result = await db.select().from(tossTable);
+  const result = await db().select().from(tossTable);
   return result;
 };
 
 export const insert = async (body: { stockCode: string; symbol: string }) => {
-  const result = await db.insert(tossTable).values([body]);
+  const result = await db().insert(tossTable).values([body]);
   return result;
 };
